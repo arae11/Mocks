6f2dd330c88002a88ce2dd219ba41c41f19d2d77
Alex Rae <arae@spartaglobal.com>
2021-09-09 10:39:32 +0100

    Added Customer Service Interface

---
+++ CustomerApp_RefactorStarter/NorthwindData/Services/CustomerService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindData.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerService(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public CustomerService()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public void CreateCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomerById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomerList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCustomer(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCustomerChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
